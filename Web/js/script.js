a0d4eb051e8e6b3487a918105c048f7bccfd938c
galvarezz09 <125933069+galvarezz09@users.noreply.github.com>
2024-09-14 22:15:23 -0500

    cambios 14092024

--- Web/js/script.js
+++ Web/js/script.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menu-toggle');
+  const navList = document.getElementById('nav-list');
+
+  menuToggle.addEventListener('click', () => {
+      navList.classList.toggle('active');
+  });
+});
+
+
 // Validación del formulario de contacto
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
