d3241c07be88225d1e32a6b660f2fcbd9d7289ef
Juan Esteban Perea Olivares <jeperea45@gmail.com>
2024-12-10 20:18:39 -0500

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/NovaDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "Sistemas3321*/";
 
     public static Connection getConnection() {
         Connection connection = null;
